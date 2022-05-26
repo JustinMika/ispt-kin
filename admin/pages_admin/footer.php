c58dead3f5f53cdbb5325d7c5b63b2e8a3753ffe
Justin Micah <jmika734@gmail.com>
2022-05-26 16:07:06 +0200

    footer.php

--- admin/pages_admin/footer.php
+++ admin/pages_admin/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span class="text-gray-800">Copyright &copy; UNIGOM BC 2022-<?=date("Y")?></span>
+            <span class="text-gray-800">Copyright &copy; ISPT-KIN BC 2022-<?=date("Y")?></span>
         </div>
     </div>
 </footer>
